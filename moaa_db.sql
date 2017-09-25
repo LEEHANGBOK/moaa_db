7690f44716873ae54f49696aaaf4b2d5426772d4
andrew <andrew@bluewisesoft.com>
2017-09-25 18:21:37 +0900

    moaa, not moaa_db

--- moaa_db.sql
+++ moaa_db.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
 --
--- Host: localhost    Database: moaa_db
+-- Host: localhost    Database: moaa
 -- ------------------------------------------------------
 -- Server version	5.7.19-0ubuntu0.16.04.1
 
@@ -14,3 +14,125 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `drives`
+--
+
+DROP TABLE IF EXISTS `drives`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `drives` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `drive_name` varchar(30) NOT NULL,
+  `drive_url` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `drive_id_UNIQUE` (`id`),
+  UNIQUE KEY `drive_name_UNIQUE` (`drive_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `drives`
+--
+
+LOCK TABLES `drives` WRITE;
+/*!40000 ALTER TABLE `drives` DISABLE KEYS */;
+/*!40000 ALTER TABLE `drives` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `token`
+--
+
+DROP TABLE IF EXISTS `token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `drive` varchar(30) NOT NULL,
+  `token` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`,`user_id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_token_1_idx` (`user_id`),
+  KEY `fk_token_drives1_idx` (`drive`),
+  CONSTRAINT `fk_token_drives` FOREIGN KEY (`drive`) REFERENCES `drives` (`drive_name`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_token_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `token`
+--
+
+LOCK TABLES `token` WRITE;
+/*!40000 ALTER TABLE `token` DISABLE KEYS */;
+/*!40000 ALTER TABLE `token` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `user_name` varchar(20) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `last_update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `user_email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'null','null','null','2017-09-25 08:58:38'),(2,'leehangbok2009@gmail.com','hangbok','12345','2017-09-25 08:59:00'),(3,'sunmin@gmail.com','sunmin','12345','2017-09-25 09:02:02');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_domain`
+--
+
+DROP TABLE IF EXISTS `users_domain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_domain` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `domain_path` varchar(200) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`users_id`),
+  KEY `fk_users_domain_users1_idx` (`users_id`),
+  CONSTRAINT `fk_users_domain_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_domain`
+--
+
+LOCK TABLES `users_domain` WRITE;
+/*!40000 ALTER TABLE `users_domain` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_domain` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-09-25 18:21:09
