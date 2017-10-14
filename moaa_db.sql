1a5de6d10f9f6b6e64da11ddb40d4fe772188b77
andrew <andrew@bluewisesoft.com>
2017-10-15 02:40:30 +0900

    update recently

--- moaa_db.sql
+++ moaa_db.sql
@@ -64,7 +64,7 @@ CREATE TABLE `token` (
   KEY `fk_token_drives1_idx` (`drive`),
   CONSTRAINT `fk_token_drives` FOREIGN KEY (`drive`) REFERENCES `drives` (`drive_name`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_token_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,7 +73,6 @@ CREATE TABLE `token` (
 
 LOCK TABLES `token` WRITE;
 /*!40000 ALTER TABLE `token` DISABLE KEYS */;
-INSERT INTO `token` VALUES (29,3,'google','ya29.GlzhBHDtiMbc3AOMbuniNwZlPd4KdyeJI6mO8St0mIyq3vCOWQR1nleP7BdmwK8bGuZpSD6aJLjKkQFUSTuBeWhpdFhk_I9181cY_VRKjj8se-3t9kuGdiaB5GqjVQ',NULL,1),(30,3,'dropbox','kFb_ENWtmyUAAAAAAAAAvFBcTIFGgBJ7T7Zx180x7g76iD0ESOdM2SG0gxfYAb8n',NULL,1),(31,3,'box','bi27uye1Ejuf6rmhOJubmgjiZEQhdBLL',NULL,1),(32,4,'google','ya29.GlzhBIuVjj-1VI80mMqmrM74EMrjvSO1QuEETY1fKUXh0mDPH-U5uP-n95hXMQwCzoDyB1TuMGQQhcFIUwVbVbC0VG_IB2EGNNmGWqznILVXkYXkn3JQu3nJSZWBAQ',NULL,1),(33,5,'google','ya29.GlzhBNe4YzpQmMVvwuqFdSAG8E_6rBRhuWZK-7zmXMu8bxWl-y3RwTY6ZRuaMy3aQheGn4nh3wgf1Kjto_0dOL8fW1xPq6EUqhEq_IikwlOcQFj5mJcwrtKuj2s8eA',NULL,1),(34,7,'google','ya29.GlzjBF55eN0QzYLJKdNX1OL5NU5UiuaDhPqUqxlljcL2PMmKfjeQNvlwAxCP2crxqprT_isxlUtwQDNc6HXkiM0Lr9ahNlc8odkqeo_po3d0DmzfCa4r1IZ68xpoFA',NULL,1),(35,7,'dropbox','KeJ5HTyanRAAAAAAAAAACTpBMp_IhJsLwOuf-REg5MtRURqSQXjE5GT-Ri-A1Eb3',NULL,1),(36,7,'box','TpzpwI0W1aTPcrk3l5Gh2ipCZRk2ShGs',NULL,1),(37,1,'google','ya29.GlzjBBwT9FycFcUxjNYCYxejwk4209n92zy3NdAwoJy2LGQB961aOkapcTX3UG0zCtxrLPt2RJkjguThjnm72VZP7lA40l9ByDPSleNz3sWy-bmrfQMS1SSG-X_1rw',NULL,1),(38,1,'dropbox','KeJ5HTyanRAAAAAAAAAADDBgSRxnCgwG6rkQI3-ncmNeogyF4Aze2aYzdSikgcyw',NULL,1),(39,1,'box','eB5JpvFNSPfJatKuiDR3cy1HnB1oRTJd',NULL,1);
 /*!40000 ALTER TABLE `token` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -93,7 +92,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `user_email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,7 +101,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'test@gmail.com','Test User','test123','2017-10-10 10:24:36'),(2,'test2@gmail.com','Test User','test123','2017-10-10 12:14:47'),(3,'sunmin@gmail.com','SSSunmin','test123','2017-10-11 13:36:50'),(4,'example1@gmail.com','test','test123','2017-10-11 17:39:36'),(5,'example2@gmail.com','test','test123','2017-10-11 17:42:03'),(6,'example3@gmail.com','Example','test123','2017-10-11 18:14:30'),(7,'speedgyuri@naver.com','Gyuriiii','test123','2017-10-13 06:20:10');
+INSERT INTO `users` VALUES (1,'test@gmail.com','Test User','test123','2017-10-14 17:39:03');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +119,7 @@ CREATE TABLE `users_domain` (
   PRIMARY KEY (`id`,`users_id`),
   KEY `fk_users_domain_users1_idx` (`users_id`),
   CONSTRAINT `fk_users_domain_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -129,7 +128,7 @@ CREATE TABLE `users_domain` (
 
 LOCK TABLES `users_domain` WRITE;
 /*!40000 ALTER TABLE `users_domain` DISABLE KEYS */;
-INSERT INTO `users_domain` VALUES (1,1,'35/a192b7913b04c54574d18c28d46e6395428ab'),(2,2,'da/b9237bacccdf19c0760cab7aec4a8359010b0'),(3,3,'77/e68daecd823babbb58edb1c8e14d7106e83bb'),(4,4,'1b/453892473a467d07372d45eb05abc2031647a'),(5,5,'ac/478d69a3c81fa62e60f5c3696165a4e5e6ac4'),(6,6,'c1/fd96eea8cc2b62785275bca38ac261256e278'),(7,7,'90/ba3cda1883801594b6e1b452790cc53948fda');
+INSERT INTO `users_domain` VALUES (1,1,'35/a192b7913b04c54574d18c28d46e6395428ab');
 /*!40000 ALTER TABLE `users_domain` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -142,4 +141,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-13 18:16:16
+-- Dump completed on 2017-10-15  2:39:45
